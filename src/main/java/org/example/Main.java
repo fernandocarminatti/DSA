765d18fc2e0084215017f1cffd8ddd80f0c02c72
fernandocarminatti <fernando.carminatti@gmail.com>
2024-10-16 11:45:26 -0300

    Add: Steps of Search Algorithms

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,15 +6,15 @@ public class Main {
     public static void main(String[] args) {
         int[] array = NumbersGenerator.getNumbersArray();
         int target = NumbersGenerator.getTarget();
+        String baseTargetResponse = "Base target number was set to -> " + target;
+        System.out.println(baseTargetResponse);
+
         int linearSearchFound = SearchAlgorithms.linearSearch(array, target);
         int binarySearchFound = SearchAlgorithms.binarySearch(array, target);
 
-        String baseTargetResponse = "Base target number was set to -> " + target;
-        String baseLinearResponse = linearSearchFound != -1 ? "Linear Search result: " + target + " at index " + linearSearchFound : "Linear Search Result: " + target + " not found.";
-        String baseBinaryResponse = binarySearchFound != -1 ? "Binary Search result: " + target + " at index " + linearSearchFound : "Binary Search Result: " + target + " not found.";
+        String linearSearchResponse = linearSearchFound != -1 ? "Linear Search result: " + target + " at index " + linearSearchFound : "Linear Search Result: " + target + " not found.";
+        String binarySearchResponse = binarySearchFound != -1 ? "Binary Search result: " + target + " at index " + binarySearchFound : "Binary Search Result: " + target + " not found.";
 
-        System.out.println(baseTargetResponse);
-        System.out.println(baseLinearResponse);
-        System.out.println(baseBinaryResponse);
+        System.out.println(linearSearchResponse);
     }
 }
--- src/main/java/org/example/search/SearchAlgorithms.java
+++ src/main/java/org/example/search/SearchAlgorithms.java
@@ -3,21 +3,36 @@ package org.example.search;
 public class SearchAlgorithms {
 
     public static int linearSearch(int[] array, int key) {
+
+        // Simple iteration on the array. O(n)
+
+        int steps = 0;
+
         for (int i = 0; i < array.length; i++) {
+            steps++;
             if (array[i] == key) {
+                System.out.println("Linear search steps: " + steps);
                 return i;
             }
         }
+        System.out.println("Linear search steps: " + steps);
         return -1;
     }
 
     public static int binarySearch(int[] array, int key) {
+        // "Divide and Conquer" on each iteration,
+        // checks for middle value and GREAT or LESS than TARGET,
+        // length of array search is
+
+        int steps = 0;
         int left = 0;
         int right = array.length - 1;
 
         while (left <= right) {
             int mid = (left + right) / 2;
+            steps++;
             if (array[mid] == key) {
+                System.out.println("Binary search steps: "+ steps);
                 return mid;
             } else if (array[mid] > key) {
                 right = mid - 1;
@@ -25,6 +40,7 @@ public class SearchAlgorithms {
                 left = mid + 1;
             }
         }
+        System.out.println("Binary search steps: "+ steps);
         return -1;
     }
 
