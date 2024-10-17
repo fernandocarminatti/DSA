86bfda367b1f6e2d93bd94d776b46994e7eb5d80
fernandocarminatti <fernando.carminatti@gmail.com>
2024-10-17 11:17:35 -0300

    Add: Insertion Sort algorithm, step count not reliable.

--- src/main/java/org/example/sorting/SortingAlgorithms.java
+++ src/main/java/org/example/sorting/SortingAlgorithms.java
@@ -1,42 +1,63 @@
 package org.example.sorting;
 
 public class SortingAlgorithms {
-    public static String bubbleSort(int[] arrayToBeSorted) {
+    public static String bubbleSort(int[] arrayToSort) {
         int steps = 0;
         int tempValue = 0;
-        int arraySize = arrayToBeSorted.length - 1;
+        int arraySize = arrayToSort.length - 1;
 
         for (int i = 0; i < arraySize; i++) {
             for (int j = 0; j < arraySize - i - 1; j++) {
                 steps++;
-                if (arrayToBeSorted[j] > arrayToBeSorted[j + 1]) {
-                    tempValue = arrayToBeSorted[j];
-                    arrayToBeSorted[j] = arrayToBeSorted[j + 1];
-                    arrayToBeSorted[j + 1] = tempValue;
+                if (arrayToSort[j] > arrayToSort[j + 1]) {
+                    tempValue = arrayToSort[j];
+                    arrayToSort[j] = arrayToSort[j + 1];
+                    arrayToSort[j + 1] = tempValue;
                 }
             }
         }
         return "Bubble Sort steps -> " + steps;
     }
 
-    public static String selectionSort(int[] arrayToBeSorted){
+    public static String selectionSort(int[] arrayToSort){
         int steps = 0;
         int minValueIndex = 0;
         int tempValue = 0;
-        int arraySize = arrayToBeSorted.length - 1;
+        int arraySize = arrayToSort.length - 1;
 
         for(int i = 0; i < arraySize; i++){
             minValueIndex = i;
             for(int j = i + 1; j < arraySize; j++){
                 steps++;
-                if(arrayToBeSorted[minValueIndex] > arrayToBeSorted[j]){
+                if(arrayToSort[minValueIndex] > arrayToSort[j]){
                     minValueIndex = j;
                 }
             }
-            tempValue = arrayToBeSorted[minValueIndex];
-            arrayToBeSorted[minValueIndex] = arrayToBeSorted[i];
-            arrayToBeSorted[i] = tempValue;
+            tempValue = arrayToSort[minValueIndex];
+            arrayToSort[minValueIndex] = arrayToSort[i];
+            arrayToSort[i] = tempValue;
         }
         return "Selection Sort steps -> " + steps;
     }
+
+    public static String insertionSort(int[] arrayToSort){
+        int steps = 0;
+        int min = 0;
+        int j = 0;
+        int arraySize = arrayToSort.length - 1;
+
+        for(int i = 1; i < arraySize; i++){
+            min = arrayToSort[i];
+            j = i - 1;
+            while( j >= 0 && arrayToSort[j] > min) {
+                arrayToSort[j+1] = arrayToSort[j];
+                j--;
+                steps++;
+            }
+            arrayToSort[j + 1] = min;
+            steps++;
+        }
+        return "Insertion sort steps -> " + steps;
+    }
+
 }
--- src/main/java/org/example/sorting/SortingMainPlayground.java
+++ src/main/java/org/example/sorting/SortingMainPlayground.java
@@ -2,17 +2,21 @@ package org.example.sorting;
 
 import org.example.NumbersGenerator;
 
-import java.util.Map;
+import java.util.Arrays;
 
 public class SortingMainPlayground {
     public static void main(String[] args) {
-        int[] arrayToBeSorted = NumbersGenerator.getUnsortedNumbersArray();
+        int[] arrayForBubbleSort = NumbersGenerator.getUnsortedNumbersArray();
+        int[] arrayForSelectionSort = NumbersGenerator.getUnsortedNumbersArray();
+        int[] arrayForInsertionSort = NumbersGenerator.getUnsortedNumbersArray();
 
-        String bubbleSortResult = SortingAlgorithms.bubbleSort(arrayToBeSorted);
-        String selectionSortResult = SortingAlgorithms.selectionSort(arrayToBeSorted);
+        String bubbleSortResult = SortingAlgorithms.bubbleSort(arrayForBubbleSort);
+        String selectionSortResult = SortingAlgorithms.selectionSort(arrayForSelectionSort);
+        String insertionSortResult = SortingAlgorithms.insertionSort(arrayForInsertionSort);
 
         System.out.println(bubbleSortResult);
         System.out.println(selectionSortResult);
+        System.out.println(insertionSortResult);
 
     }
 }
