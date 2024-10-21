ade43c09fb54bc432e10b034389dad44b50546d6
fernandocarminatti <fernando.carminatti@gmail.com>
2024-10-21 12:56:58 -0300

    Add: Readme.MD for Sorting Algorithms + Messing around with Sorting.

--- src/main/java/org/example/sorting/SortingAlgorithms.java
+++ src/main/java/org/example/sorting/SortingAlgorithms.java
@@ -3,15 +3,13 @@ package org.example.sorting;
 import java.util.Arrays;
 
 public class SortingAlgorithms {
-    /*
-    Encontrar o MAIOR valor e joga-lo par ao final da lista.
-    */
+
     public static void bubbleSort(int[] arrayToSort) {
         int tempValue = 0;
         int arraySize = arrayToSort.length - 1;
 
         for (int i = 0; i < arraySize; i++) {
-            for (int j = 0; j < arraySize - i - 1; j++) {
+            for (int j = 0; j < arraySize - i; j++) {
                 if (arrayToSort[j] > arrayToSort[j + 1]) {
                     tempValue = arrayToSort[j];
                     arrayToSort[j] = arrayToSort[j + 1];
@@ -21,10 +19,7 @@ public class SortingAlgorithms {
         }
     }
 
-    /*
-    Encontrar o MENOR valor e joga-lo par ao inicio da lista.
-    */
-    public static int[] selectionSort(int[] arrayToSort){
+    public static void selectionSort(int[] arrayToSort){
         int minValueIndex = 0;
         int tempValue = 0;
         int arraySize = arrayToSort.length - 1;
@@ -40,25 +35,19 @@ public class SortingAlgorithms {
             arrayToSort[minValueIndex] = arrayToSort[i];
             arrayToSort[i] = tempValue;
         }
-        return arrayToSort;
     }
 
-    /*
-    Encontrar o MENOR valor e separa-lo da lista. Mover os MAIORES valores para a direita até que sobre a posição para inserir o MENOR valor.
-    */
-    public static void insertionSort(int[] arrayToSort){
-        int min = 0;
-        int j = 0;
-        int arraySize = arrayToSort.length - 1;
+    public static void insertionSort(int[] array){
+        int arrSize = array.length;
 
-        for(int i = 1; i < arraySize; i++){
-            min = arrayToSort[i];
-            j = i - 1;
-            while( j >= 0 && arrayToSort[j] > min) {
-                arrayToSort[j+1] = arrayToSort[j];
+        for(int i = 0; i < arrSize; i++){
+            int min = array[i];
+            int j = i - 1;
+            while(j>=0 && array[j] > min){
+                array[j+1] = array[j];
                 j--;
             }
-            arrayToSort[j + 1] = min;
+            array[j+1] = min;
         }
     }
 
@@ -91,21 +80,22 @@ public class SortingAlgorithms {
         array[num02] = temp;
     }
 
-    public static int[] reimplementation(int[] array){
-        int temp = 0;
+    public static void reimplementation(int[] array)    {
+        int minIdx = 0;
         int arraySize = array.length - 1;
 
-        for(int i = 0; i< arraySize; i++){
-            for(int j = 0; j < arraySize - i; j++){
-                System.out.println(Arrays.toString(array));
-                if(array[j] > array[j + 1]){
-                    temp = array[j];
-                    array[j] = array[j + 1];
-                    array[j + 1] = temp;
+        for(int i = 0; i < arraySize; i++){
+            minIdx = i;
+            for(int j = i + 1; j < arraySize + 1; j++){
+                if(array[minIdx] > array[j]){
+                    minIdx = j;
                 }
             }
+            int temp = array[minIdx];
+            array[minIdx] = array[i];
+            array[i] = temp;
         }
-        return array;
     }
+
 
 }
--- src/main/java/org/example/sorting/SortingMainPlayground.java
+++ src/main/java/org/example/sorting/SortingMainPlayground.java
@@ -8,19 +8,10 @@ public class SortingMainPlayground {
     public static void main(String[] args) {
 
         int[] array = NumbersGenerator.getUnsortedNumbersArray();
-
-        // O(n²)
-        //SortingAlgorithms.bubbleSort(arrayForBubbleSort);
-        // O(n²)
-        //SortingAlgorithms.selectionSort(arrayForSelectionSort);
-        // O(n²)
-        //SortingAlgorithms.insertionSort(arrayForInsertionSort);
-        // O(n log n) ~ AVERAGE
-        // O(n²) ~ WORST.
-        int[] sorted = SortingAlgorithms.selectionSort(array);
-        //int[] sorted2 = SortingAlgorithms.selectionSort(array);
-
-        System.out.println("Sorting Done!");
-        System.out.println(Arrays.toString(sorted));
+        System.out.println(Arrays.toString(array) + " <- Original Array");
+        System.out.println();
+        SortingAlgorithms.reimplementation(array);
+        System.out.println(Arrays.toString(array) + " <- Sorted!");
+        System.out.println();
     }
 }
